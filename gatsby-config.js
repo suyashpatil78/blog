495498b5c0bb667469c8661818fc7350e3a487ab
suyashpatil78 <suyashpatil505@gmail.com>
2024-07-16 20:13:38 +0530

    add google adsense

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,5 +93,11 @@ module.exports = {
 		},
 		'gatsby-plugin-catch-links',
 		'gatsby-plugin-sitemap',
+		{
+			resolve: `gatsby-plugin-google-adsense`,
+			options: {
+				publisherId: `ca-pub-7926071698481991`
+			},
+		},
 	],
 };
